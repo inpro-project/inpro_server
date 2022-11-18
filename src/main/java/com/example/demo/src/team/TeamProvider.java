74a3acfea510200cfbd72ea4e933e8b4e5d32a82
syb0228 <syb0228@naver.com>
2022-11-18 18:32:52 +0900

    [feat] 유저가 등록한 팀 전체 조회 API

--- src/main/java/com/example/demo/src/team/TeamProvider.java
+++ src/main/java/com/example/demo/src/team/TeamProvider.java
@@ -1,10 +1,15 @@
 package com.example.demo.src.team;
 
 import com.example.demo.config.BaseException;
+import com.example.demo.src.team.model.GetTeamProfileRes;
+import com.example.demo.src.team.model.GetTeamsRes;
+import com.example.demo.src.user.model.GetUserProfileRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.example.demo.config.BaseResponseStatus.DATABASE_ERROR;
 
@@ -35,6 +40,15 @@ public class TeamProvider {
     public int checkPreTeamMember(int teamIdx, int userIdx) throws BaseException {
         try {
             return teamDao.checkPreTeamMember(teamIdx, userIdx);
+        } catch (Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    public List<GetTeamsRes> getTeams(int userIdx) throws BaseException {
+        try {
+            List<GetTeamsRes> getTeamsResList = teamDao.getTeams(userIdx);
+            return getTeamsResList;
         } catch (Exception exception){
             throw new BaseException(DATABASE_ERROR);
         }
---
+++ src/main/java/com/example/demo/src/team/model/GetTeamsRes.java
@@ -0,0 +1,27 @@
+package com.example.demo.src.team.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@ApiModel(value = "유저가 등록한 팀 전체 조회 API")
+public class GetTeamsRes {
+    @ApiModelProperty(value = "팀 인덱스", example = "1")
+    private int teamIdx;
+
+    @ApiModelProperty(value = "팀 제목", example = "인플 프로젝트")
+    private String title;
+
+    @ApiModelProperty(value = "팀 유형", example = "프로젝트")
+    private String type;
+
+    @ApiModelProperty(value = "지역", example = "인천")
+    private String region;
+
+    @ApiModelProperty(value = "분야", example = "IT/인터넷")
+    private String interests;
+}
